210601004776c5995e67362c707ce90232b254fd
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2021-10-15 22:40:28 +0700

    add mr icon

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,13 +3,13 @@ import { chakra, Box, Image, useColorMode } from '@chakra-ui/react';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import logolight from '../booklight.gif';
 import logodark from '../bookdark.gif';
-
+import { Link } from 'react-router-dom';
 function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
   const logo = colorMode === 'light' ? logolight : logodark;
   return (
     <chakra.header d="flex" justifyContent="space-between" alignItems="center">
-      <Box w={100}>
+      <Box w={100} as={Link} to="/">
         <Image src={logo}></Image>
       </Box>
       <ColorModeSwitcher
