8218bc8f7e6adff59280bb382774bf34beb9cc97
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2021-10-15 22:28:19 +0700

    deleteModalUpdate

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -6,8 +6,8 @@ function Banner() {
       Tôi ơi hãy <Code colorScheme="blue">strong </Code> lên! Hãy{' '}
       <Code colorScheme="blue">win myself</Code> của mình, sự thật thì luôn luôn
       đơn giản nhưng
-      <Code colorScheme="blue">people make it complicated</Code>, nên là mình cứ{' '}
-      <Code colorScheme="blue">enjoy</Code> cái{' '}
+      <Code colorScheme="blue"> people make it complicated</Code>, nên là mình
+      cứ <Code colorScheme="blue">enjoy</Code> cái{' '}
       <Code colorScheme="blue">moment</Code> này, cố gắng{' '}
       <Code colorScheme="blue">smile</Code> khi gặp{' '}
       <Code colorScheme="blue"> hard</Code> và{' '}
