58c7e1001e2cae89027bc073b98cd37ab97e40ea
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2021-10-16 21:16:01 +0700

    count down and timer

---
+++ src/component/CountDown.jsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Box, Button, Input, Heading } from '@chakra-ui/react';
+function CountDown() {
+  const [time, setTime] = useState(0);
+  const [isPlay, setIsPlay] = useState(false);
+  const [isPause, setIsPause] = useState(false);
+  const [timeNotFormat, setTimeNotFormat] = useState({
+    hour: 0,
+    minute: 0,
+    second: 0,
+  });
+  const [isCountDown, setIsCountDown] = useState(false);
+  const coutDownTime = useRef();
+  // const unCountTime = useRef();
+
+  useEffect(() => {
+    return () => {
+      setTime(0);
+      clearInterval(coutDownTime.current);
+    };
+  }, []);
+  useEffect(() => {
+    if (time === 0) {
+      clearInterval(coutDownTime.current);
+      setIsCountDown(false);
+    }
+  }, [time]);
+  const formatTime = timer => {
+    const getSeconds = `0${timer % 60}`.slice(-2);
+    const minutes = `${Math.floor(timer / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
+  const formatTimeToSecond = ({ hour, minute, second }) => {
+    return hour * 3600 + minute * 60 + second;
+  };
+
+  const handleClickPlay = () => {
+    if (!isPlay) {
+      coutDownTime.current = setInterval(() => {
+        setTime(time => time - 1);
+      }, 1000);
+    }
+    setIsPlay(true);
+    setIsPause(false);
+  };
+
+  const handleClickPause = () => {
+    clearInterval(coutDownTime.current);
+    setIsPause(true);
+    setIsPlay(false);
+  };
+
+  const handleClickReset = () => {
+    clearInterval(coutDownTime.current);
+    setTime(0);
+    setIsPlay(false);
+    setIsCountDown(false);
+  };
+
+  const handleChangCountDown = e => {
+    setTimeNotFormat({ ...timeNotFormat, [e.target.name]: +e.target.value });
+  };
+  const handleCountDownStart = () => {
+    console.log('ahihi');
+
+    if (!isCountDown) {
+      console.log('ahihi');
+      const timeFormatSecond = formatTimeToSecond(timeNotFormat);
+      setTime(timeFormatSecond);
+      handleClickPlay();
+      console.log(timeFormatSecond);
+      setTimeNotFormat({
+        hour: 0,
+        minute: 0,
+        second: 0,
+      });
+      setIsCountDown(true);
+    }
+  };
+
+  return (
+    <Box>
+      <Box>
+        <Heading
+          d="inline-block"
+          pl={5}
+          pr={5}
+          borderRadius={10}
+          bg="rgba(255, 255, 255, 0.2)"
+          fontSize={{ base: '50px', md: '70px', lg: '100px' }}
+        >
+          {formatTime(time)}
+        </Heading>
+      </Box>
+      <Box mt={5}>
+        <Button
+          color="white"
+          bg="rgba(255, 255, 255, 0.2)"
+          mr={5}
+          onClick={handleClickPlay}
+          _hover={{ bg: 'rgba(255, 255, 255, 0.3)' }}
+        >
+          Play
+        </Button>
+
+        <Button
+          color="white"
+          bg="rgba(255, 255, 255, 0.2)"
+          mr={5}
+          _hover={{ bg: 'rgba(255, 255, 255, 0.3)' }}
+          onClick={handleClickPause}
+        >
+          Pause
+        </Button>
+        <Button
+          color="white"
+          bg="rgba(255, 255, 255, 0.2)"
+          _hover={{ bg: 'rgba(255, 255, 255, 0.3)' }}
+          mr={5}
+          onClick={handleClickReset}
+        >
+          Reset
+        </Button>
+      </Box>
+      <Box d="flex" maxW={300} m="0 auto" mt={5}>
+        <Input
+          type="number"
+          name="hour"
+          mr={5}
+          placeholder="Hour"
+          value={timeNotFormat.hour}
+          onChange={e => handleChangCountDown(e)}
+        />
+        <Input
+          mr={5}
+          name="minute"
+          placeholder="Minute"
+          onChange={e => handleChangCountDown(e)}
+          value={timeNotFormat.minute}
+        />
+        <Input
+          name="second"
+          placeholder="Second"
+          onChange={e => handleChangCountDown(e)}
+          value={timeNotFormat.second}
+        />
+      </Box>
+      <Button
+        color="white"
+        bg="rgba(255, 255, 255, 0.2)"
+        _hover={{ bg: 'rgba(255, 255, 255, 0.3)' }}
+        onClick={handleCountDownStart}
+        mt={5}
+      >
+        Start
+      </Button>
+    </Box>
+  );
+}
+
+export default CountDown;
