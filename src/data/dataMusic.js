11994e092d8d36e63e3a77cc1976d2d8f529393b
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2021-10-16 23:14:18 +0700

    avatar and music

---
+++ src/data/dataMusic.js
@@ -0,0 +1,131 @@
+export const music1 = [
+  {
+    name: 'Dusk Till Dawn',
+    time: 0,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: "Can't Take My Eyes Off You",
+    time: 218,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'Young Dumb & Broke',
+    time: 380,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'Perfect',
+    time: 500,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'Killing Me Softly',
+    time: 692,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'Girls Like You',
+    time: 858,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: "Can't Help Falling in Love",
+    time: 1045,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+];
+
+export const music2 = [
+  {
+    name: 'You Are The Reason',
+    time: 1257,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'Love Me Like You Do',
+    time: 1457,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'All Falls Down',
+    time: 1677,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'The One That Got Away',
+    time: 1832,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'One Call Away',
+    time: 2078,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'As long as you love me',
+    time: 2270,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+  {
+    name: 'Locked Away',
+    time: 2437,
+    author: {
+      name: 'ZAYN ft. Sia',
+      image:
+        'https://www.thewikifeed.com/wp-content/uploads/2021/04/zayn-malik-1.jpg',
+    },
+  },
+];
